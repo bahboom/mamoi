76bf15b60ee06e1c4e390478c0aca4b590de33dc
Ming Liu <nongnongd@yahoo.com>
2013-05-01 01:14:46 -0400

    Added skeleton main class for IsoMaker

---
+++ isomaker/src/main/java/com/mcode/mamoi/isomaker/IsoMaker.java
@@ -0,0 +1,13 @@
+package com.mcode.mamoi.isomaker;
+
+public class IsoMaker {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
