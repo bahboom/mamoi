7e921dc78b9de2c2df4dddbb8eca408cd4484d97
Ming <nongnongd@yahoo.com>
2013-05-18 16:36:47 -0400

    Added comment functionality

--- mcc/src/main/java/com/mcode/mamoi/mcc/mcc.java
+++ mcc/src/main/java/com/mcode/mamoi/mcc/mcc.java
@@ -20,36 +20,39 @@ public class mcc {
 		String line = br.readLine();
 		int bytesWritten = 0;
 		while ( line != null ) {
-			String[] elements = line.split( " " );
-			for(int i = 0; i < elements.length; i++) {
-				String element = elements[i];
-				// Check for modes
-				if(element.equals("padzeros")) {
-					padzeros = true;
-					continue;
-				} else if ( element.equals("hex") ) {
-					radix = 16;
-					continue;
-				} else if ( element.equals("decimal")) {
-					radix = 10;
-					continue;
-				} else if ( element.equals("binary")) {
-					radix = 2;
-					continue;
-				}
-				
-				if(padzeros) { // pad zeros until byte number specified
-					while( bytesWritten < Integer.parseInt(element, radix) ) {
-						fos.write(0);
+			line = line.replaceAll(";.*", ""); // remove comments from code
+			if(!line.trim().isEmpty()) {
+				String[] elements = line.split( " " );
+				for(int i = 0; i < elements.length; i++) {
+					String element = elements[i];
+					// Check for modes
+					if(element.equals("padzeros")) {
+						padzeros = true;
+						continue;
+					} else if ( element.equals("hex") ) {
+						radix = 16;
+						continue;
+					} else if ( element.equals("decimal")) {
+						radix = 10;
+						continue;
+					} else if ( element.equals("binary")) {
+						radix = 2;
+						continue;
+					}
+					
+					if(padzeros) { // pad zeros until byte number specified
+						while( bytesWritten < Integer.parseInt(element, radix) ) {
+							fos.write(0);
+							bytesWritten ++;
+						}
+						padzeros = false;
+					} else { // normal mode
+						fos.write(Integer.parseInt(element, radix));
 						bytesWritten ++;
 					}
-					padzeros = false;
-				} else { // normal mode
-					fos.write(Integer.parseInt(element, radix));
-					bytesWritten ++;
 				}
-
 			}
+
 			line = br.readLine();
 		}
 		fos.close();
