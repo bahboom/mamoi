10ee8f229a74b10cd2266dacf9be751bc64afdcf
Ming Liu <nongnongd@yahoo.com>
2013-05-18 14:55:09 -0400

    mcc initial check in

---
+++ mcc/src/main/java/com/mcode/mamoi/mcc/mcc.java
@@ -0,0 +1,13 @@
+package com.mcode.mamoi.mcc;
+
+/**
+ * Hello world!
+ *
+ */
+public class mcc 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
