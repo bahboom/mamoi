755c7eefbae346dfb2840d400c1ac7711f956b87
Ming <nongnongd@yahoo.com>
2013-05-18 19:07:32 -0400

    Added feature to define user commands

--- mcc/src/main/java/com/mcode/mamoi/mcc/mcc.java
+++ mcc/src/main/java/com/mcode/mamoi/mcc/mcc.java
@@ -5,61 +5,150 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class mcc {
+	private int lastRadix = 10;
+	private int radix = 10;
+	private boolean padding = false;
+	
+	private boolean defining = false;
+	private String definitionName = null;
+	private List<String> elements = new ArrayList<String>();
+	private Map<String, List<String>> userdefinedCommands = new HashMap<String, List<String>>(); 
+	
+	public mcc() {
+		
+	}
+	
+	public void addElement(String e) throws Exception {
+		e = e.toLowerCase();
+		
+		if(e.equals("def")) {
+			if(defining) {
+				throw new Exception("error: already defining");
+			}
+			defining = true;
+			return;
+		} else if (e.equals("edef")) {
+			if(!defining) {
+				throw new Exception("error: not defining");
+			}
+			defining = false;
+			definitionName = null;
+			return;
+		} 
+		
+		if(defining) {
+			if(definitionName == null) {
+				if(userdefinedCommands.containsKey(e)) {
+					throw new Exception("error: definition already exist");
+				}
+				definitionName = e;
+				userdefinedCommands.put(e, new ArrayList<String>());
+			} else {
+				userdefinedCommands.get(definitionName).add(e);
+			}
+			
+		} else {
+			elements.add(e);
+		}
+	}
+	
+	private void expandDefinitions() {
+		
+		while(containsUserDefinition()) {
+			List<String> expandedElements = new ArrayList<String>();
+			for(String element : elements) {
+				if(userdefinedCommands.containsKey(element)) {
+					expandedElements.addAll(userdefinedCommands.get(element));
+				} else {
+					expandedElements.add(element);
+				}
+			}
+			
+			elements = expandedElements;
+			
+		}
+	}
+	
+	private boolean containsUserDefinition() {
+		for(String element : elements) {
+			if(userdefinedCommands.containsKey(element))
+				return true;
+		}
+		return false;
+	}
+	
+	public int compile(String binaryFile) throws Exception {
+		expandDefinitions();
+		int bytesWritten = 0;
+		OutputStream fos = new FileOutputStream(binaryFile);
+		
+		
+		for(String element : elements) {
+			if("pz".equals(element)) {
+				padding = true;
+				continue;
+			} else if("hex".equals(element)) {
+				lastRadix = radix;
+				radix = 16;
+				continue;
+			} else if("bin".equals(element)) {
+				lastRadix = radix;
+				radix = 2;
+				continue;
+			} else if("dec".equals(element)) {
+				lastRadix = radix;
+				radix = 10;
+				continue;
+			} else if("lr".equals(element)) {
+				int tmp = radix;
+				radix = lastRadix;
+				lastRadix = tmp;
+				continue;
+			}
+			
+			if(padding) {
+				while(bytesWritten < Integer.parseInt(element)) {
+					fos.write(0);
+					bytesWritten++;
+				}
+				padding = false;
+				continue;
+			}
+			
+			fos.write(Integer.parseInt(element, radix));
+			bytesWritten++;
+		}
+		fos.close();
+		return bytesWritten;
+	}
+	
     public static void main( String[] args ) throws Exception {
-    	// modes
-    	boolean padzeros = false;
-    	int radix = 10;
-    	
     	String sourceFile = args[0];
     	String binaryFile = args[1];
-		OutputStream fos = new FileOutputStream(binaryFile);
+    	mcc compiler = new mcc();
 		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(sourceFile)));
 		
 		String line = br.readLine();
-		int bytesWritten = 0;
 		while ( line != null ) {
 			line = line.replaceAll(";.*", ""); // remove comments from code
 			if(!line.trim().isEmpty()) {
 				String[] elements = line.split( " " );
 				for(int i = 0; i < elements.length; i++) {
 					String element = elements[i];
-					if(element.trim().isEmpty()) {
-						continue;
-					}
-					// Check for modes
-					if(element.equalsIgnoreCase("pz")) {
-						padzeros = true;
-						continue;
-					} else if ( element.equalsIgnoreCase("hex") ) {
-						radix = 16;
-						continue;
-					} else if ( element.equalsIgnoreCase("dec")) {
-						radix = 10;
-						continue;
-					} else if ( element.equalsIgnoreCase("bin")) {
-						radix = 2;
-						continue;
-					}
-					
-					if(padzeros) { // pad zeros until byte number specified
-						while( bytesWritten < Integer.parseInt(element, radix) ) {
-							fos.write(0);
-							bytesWritten ++;
-						}
-						padzeros = false;
-					} else { // normal mode
-						fos.write(Integer.parseInt(element, radix));
-						bytesWritten ++;
+					if(!element.trim().isEmpty()) {
+						compiler.addElement(element);
 					}
 				}
 			}
-
 			line = br.readLine();
 		}
-		fos.close();
 		br.close();
-		System.out.println("Bytes written: " + bytesWritten);
+    	System.out.println("Bytes written: " + compiler.compile(binaryFile));
     }
 }
