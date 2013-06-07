357e1b78a248c05e1dd9aef6f145f62196644355
Ming Liu <nongnongd@yahoo.com>
2013-06-07 16:45:34 -0400

    Made improvements to syntax highlighting

--- mcide/src/main/java/com/mcode/mamoi/mcide/mcide.java
+++ mcide/src/main/java/com/mcode/mamoi/mcide/mcide.java
@@ -4,6 +4,8 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -18,18 +20,25 @@ import javax.swing.text.StyledDocument;
 public class mcide extends JPanel {
 	private JTextPane mainTextPane = null;
 	private StyledDocument doc = null;
-	private Style plainStyle = null;
-	private Style keywordStyle = null;
 	private mcideStatusPanel statusPanel = null;
-	
-	
+	private Set<String> mccKeywords = null;
 	
 	public mcide() {
+		mccKeywords = new HashSet<String>();
+		mccKeywords.add("pz");
+		mccKeywords.add("def");
+		mccKeywords.add("edef");
+		mccKeywords.add("include");
+		mccKeywords.add("hex");
+		mccKeywords.add("bin");
+		mccKeywords.add("dec");
+		mccKeywords.add("str");
+		mccKeywords.add("lr");
 		setLayout(new BorderLayout());
 		doc = (StyledDocument) new DefaultStyledDocument();
 		mainTextPane = new JTextPane(doc);
-		plainStyle = mainTextPane.addStyle("plain", null);
-		keywordStyle = mainTextPane.addStyle("keyword", null);
+		Style plainStyle = mainTextPane.addStyle("plain", null);
+		Style keywordStyle = mainTextPane.addStyle("keyword", null);
 		StyleConstants.setForeground(keywordStyle, Color.MAGENTA);
 		StyleConstants.setBold(keywordStyle, true);
 		
@@ -44,17 +53,7 @@ public class mcide extends JPanel {
 			}
 
 			public void keyTyped(KeyEvent e) {
-				//doc.re
-				doc.setParagraphAttributes(0, doc.getLength(), mainTextPane.getStyle("plain"), true);
-				String text = mainTextPane.getText();
-				//mainTextPane.getHighlighter().;
-				int i = text.indexOf("def");
-				while(i != -1) {
-					doc.setCharacterAttributes(i, 3, mainTextPane.getStyle("keyword"), true);
-					i = text.indexOf("def", i+1);
-				}
-				
-				doc.setParagraphAttributes(0, doc.getLength(), mainTextPane.getStyle("plain"), true);
+				walk(doc);
 			}
 			
 		});
@@ -64,6 +63,22 @@ public class mcide extends JPanel {
         mainScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 		this.add(mainScrollPane, BorderLayout.CENTER);
 		this.add(statusPanel, BorderLayout.SOUTH);
+	}
+	
+	private void walk(StyledDocument doc) {
+		// reset all syntax highlighting
+		doc.setCharacterAttributes(0, doc.getLength() + 1, mainTextPane.getStyle("plain"), true);
+		
+		String text = mainTextPane.getText();
+		for(String keyword : mccKeywords) {
+		int i = text.indexOf(keyword);
+			while(i != -1) {
+				doc.setCharacterAttributes(i, keyword.length(), mainTextPane.getStyle("keyword"), true);
+				i = text.indexOf(keyword, i + 1);
+			}
+		}
+		
+		//doc.setParagraphAttributes(0, doc.getLength(), mainTextPane.getStyle("plain"), true);
 	}
 	
 	public static void main(String args[]) {
